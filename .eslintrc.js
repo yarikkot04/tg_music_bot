8c5d7c296eb86b59e344fd04ada33fbd2444c9c3
yarikkot04 <yarikkotenkoim13@gmail.com>
2024-04-30 02:31:00 +0900

    Configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['semistandard', 'standard'],
+  rules: {
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'never',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'new-cap': 'off'
+  }
+}
