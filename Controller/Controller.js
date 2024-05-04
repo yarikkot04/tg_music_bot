184b113bb0aa71793f692d0841f735ed053972be
yarikkot04 <yarikkotenkoim13@gmail.com>
2024-05-04 23:52:00 +0900

    Added methods for selecting download format and download method (by name, by link).

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -12,6 +12,10 @@ export default class Controler {
       this.start()
       this.setLanguageHandlers()
       this.chooseLang()
+      this.setDownloadFormatHandlers()
+      this.chooseDownloadFormat()
+      this.setDownloadMethodHandlers()
+      this.chooseDownloadMethod()
       this.bot_launch()
       this.stop()
     } catch (e) {
@@ -74,6 +78,75 @@ export default class Controler {
     user.loc = lang
     await user.save()
     ctx.telegram.sendMessage(user.userTgId, messages[user.loc].chatLang)
+  }
+
+  chooseDownloadFormat() {
+    try {
+      this._bot.command('choose_format', async (ctx) => {
+        const user = await User.findOne({ userTgId: ctx.chat.id })
+        ctx.reply(
+          messages[`${user.loc}`].chooseFormat,
+          Markup.inlineKeyboard([
+            [Markup.button.callback('MP3', 'format_mp3')],
+            [Markup.button.callback('MP4', 'format_mp4')]
+          ])
+        )
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  async setDownloadFormatHandlers() {
+    this._handleDownloadAction('format_mp3', 'MP3', this._setDownloadFormat)
+    this._handleDownloadAction('format_mp4', 'MP4', this._setDownloadFormat)
+  }
+
+  async _setDownloadFormat(format, user, ctx) {
+    user.downloadFormat = format
+    await user.save()
+    ctx.telegram.sendMessage(user.userTgId, messages[user.loc].downloadFormat + user.downloadFormat)
+  }
+
+  chooseDownloadMethod() {
+    try {
+      this._bot.command('choose_method', async (ctx) => {
+        const user = await User.findOne({ userTgId: ctx.chat.id })
+        ctx.reply(
+          messages[`${user.loc}`].chooseMethod,
+          Markup.inlineKeyboard([
+            [Markup.button.callback(messages[user.loc].downloadMethods[0], 'name')],
+            [Markup.button.callback(messages[user.loc].downloadMethods[1], 'link')]
+          ])
+        )
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  async setDownloadMethodHandlers() {
+    this._handleDownloadAction('name', 'Name', this._setDownloadMethod)
+    this._handleDownloadAction('link', 'Link', this._setDownloadMethod)
+  }
+
+  async _setDownloadMethod(method, user, ctx) {
+    user.downloadMethod = method
+    await user.save()
+    if (user.loc === 'ukr' && user.downloadMethod === 'Name') {
+      ctx.telegram.sendMessage(user.userTgId, messages[user.loc].downloadMethod + messages.ukr.downloadMethods[0])
+    } else if (user.loc === 'ukr' && user.downloadMethod === 'Link') {
+      ctx.telegram.sendMessage(user.userTgId, messages[user.loc].downloadMethod + messages.ukr.downloadMethods[1])
+    } else {
+      ctx.telegram.sendMessage(user.userTgId, messages[user.loc].downloadMethod + user.downloadMethod)
+    }
+  }
+
+  async _handleDownloadAction(action, value, handler) {
+    this._bot.action(action, async (ctx) => {
+      const user = await User.findOne({ userTgId: ctx.chat.id })
+      handler.call(this, value, user, ctx)
+    })
   }
 
   async bot_launch() {
--- Controller/messages.js
+++ Controller/messages.js
@@ -4,13 +4,23 @@ const messages = {
     greetings: 'Hello! You are greeted by Everain_Music_bot. Here you can find music for every taste. For a more detailed description of the bot\'s capabilities, use the command /help.',
     chooseLanguage: 'Choose your language:',
     languages: ['English', 'Ukrainian'],
-    chatLang: 'Chat language: English'
+    chatLang: 'Chat language: English',
+    chooseFormat: 'Choose a format for download: ',
+    downloadFormat: 'Download format: ',
+    chooseMethod: 'Choose a download method:',
+    downloadMethods: ['Name', 'Link'],
+    downloadMethod: 'Download method: '
   },
   ukr: {
     greetings: 'Привіт! Тебе вітає Everain_Music_bot. Тут ти зможеш знайти музику на будь-який смак. Для більш детального опису можливостей бота скористайся командою /help.',
     chooseLanguage: 'Виберіть мову:',
     languages: ['Англійська', 'Українська'],
-    chatLang: 'Мова чату : Українська'
+    chatLang: 'Мова чату : Українська',
+    chooseFormat: 'Виберіть формат для завантаження: ',
+    downloadFormat: 'Формат скачування: ',
+    chooseMethod: 'Оберіть метод скачування:',
+    downloadMethods: ['Ім\'я', 'Посилання'],
+    downloadMethod: 'Метод скачування: '
   }
 }
 
